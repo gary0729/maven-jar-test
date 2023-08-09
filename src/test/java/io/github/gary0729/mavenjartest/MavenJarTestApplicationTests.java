cd01bcc5a34a551b694d59208bad515fc72b15d5
g30044254 <gaojian102@huawei.com>
2023-08-09 09:16:12 +0800

    first commit

---
+++ src/test/java/io/github/gary0729/mavenjartest/MavenJarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gary0729.mavenjartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
