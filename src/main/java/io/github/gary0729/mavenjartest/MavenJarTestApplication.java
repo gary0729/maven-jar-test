86c6eab236b6a289227e90843ebc667302d5a9f6
g30044254 <gaojian102@huawei.com>
2023-09-11 20:21:24 +0800

    init

---
+++ src/main/java/io/github/gary0729/mavenjartest/MavenJarTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.gary0729.mavenjartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJarTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenJarTestApplication.class, args);
+    }
+
+}
